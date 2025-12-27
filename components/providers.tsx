2448de9c5038e8f13682c9b02ddd1f257f865b62
StreallyX <aproject37@gmail.com>
2025-12-27 14:50:40 +0100

    changes

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,27 +1,19 @@
 'use client';
 
 import { ThemeProvider } from '@/components/theme-provider';
-import { useEffect, useState } from 'react';
+import { SessionProvider } from 'next-auth/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      enableSystem={false}
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem={false}
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
