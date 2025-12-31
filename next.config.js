526d87a1110fcebbd287fea26e768fc68a3028ea
StreallyX <aproject37@gmail.com>
2025-12-31 12:33:44 +0100

    fix(next.config)

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-const path = require('path');
 const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+
+  // ⚠️ SUPPRIMER output + outputFileTracingRoot
+  // output: process.env.NEXT_OUTPUT_MODE,
+
   eslint: {
     ignoreDuringBuilds: true,
   },
